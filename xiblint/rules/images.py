9c5402051a1a3229d1cebc3156a3397af6f265ab
Brad Smith <bradley.d.smith11@gmail.com>
2019-03-28 13:22:31 -0400

    Update images.py
    Update check for LaunchScreens

--- xiblint/rules/images.py
+++ xiblint/rules/images.py
@@ -6,7 +6,7 @@ class Images(Rule):
     Checks for images
     """
     def check(self, context):  # type: (Rule, xiblint.xibcontext.XibContext) -> None
-        if ("LaunchScreen-" not in context.path):
+        if ("LaunchScreen" not in context.path):
             for image in context.tree.findall("./resources/image"):
                 title = image.get("name")
                 context.error(image, "Image exists with name: {}", title)
